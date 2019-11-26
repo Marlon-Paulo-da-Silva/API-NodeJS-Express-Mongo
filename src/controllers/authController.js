13f3a615efb6e012b41dbd872baae07eb0d356a9
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-11-26 02:39:46 -0300

    rodando o cadastro de usuario

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,12 +5,19 @@ const User = require("../models/User");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  const { email } = req.body;
+
   try {
+    if (await User.findOne({ email }))
+      return res.status(400).send({ error: "Usuario ja existe" });
+
     const user = await User.create(req.body);
 
+    user.password = undefined;
+
     return res.send({ user });
-  } catch (err) {
-    return res.status(400).send({ error: "Falha no Registro" });
+  } catch (error) {
+    return res.status(400).send({ error: "Falha no registro" });
   }
 });
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/noderest", { useMongoClient: true });
-mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0-beyum.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => {
+    console.log("banco de dados conectado");
+  }
+);
 
 module.exports = mongoose;
