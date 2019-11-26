a2c8ad8131311a19f4b45203e79b1cda191a2c95
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-11-25 23:34:36 -0300

    teste 1 funcionamento

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+//indicar que vai usar o bodyparser - a funçao json
+app.use(bodyParser.json());
+//entender quando passar parametros via url
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//recebe sempre o req e res
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(3000);
